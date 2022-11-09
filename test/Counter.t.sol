aeefa65df85697e736c50932772dc658fb359f81
Will Jiang <willjiang2018@gmail.com>
2022-11-09 15:16:06 -0800

    update test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,22 +3,38 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import "../src/AMB.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Counter public sendingCounter;
+    AMB public sendingAMB;
+    Counter public receivingCounter;
+    AMB public receivingAMB;
+
+    address public relayer = address(0x1);
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        sendingAMB = new AMB(relayer);
+        sendingCounter = new Counter(address(sendingAMB));
+        receivingAMB = new AMB(relayer);
+        receivingCounter = new Counter(address(receivingAMB));
+        vm.startPrank(relayer);
+
+        sendingAMB.setRecipientAMB(address(receivingAMB));
+        receivingAMB.setRecipientAMB(address(sendingAMB));
+        sendingCounter.setReceivingCounter(address(receivingCounter));
+        receivingCounter.setReceivingCounter(address(sendingCounter));
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testSend() public {
+        bytes memory sendingResult1 = sendingCounter.send();
+        bytes memory sendingResult2 = sendingAMB.send(address(receivingCounter), abi.encodeWithSignature("increment()"));
+        assertEq(sendingResult1, sendingResult2);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testReceive() public {
+        bytes memory sendingResult = sendingCounter.send();
+        receivingAMB.receive(sendingResult);
+        assertEq(receivingCounter.counter(), 1);
     }
 }
